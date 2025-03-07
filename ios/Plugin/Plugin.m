dcb20d8d3b4a0d8de83a247c35984fb78fe65996
Nik Mohd Faris <nikfaris88@gmail.com>
2025-03-08 06:05:46 +0800

    Fix bridging

--- ios/Plugin/Plugin.m
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-// Register FiuuPaymentPlugin with all its methods
-CAP_PLUGIN(FiuuPaymentPlugin, "FiuuPaymentPlugin",
-    CAP_PLUGIN_METHOD(startPayment, CAPPluginReturnPromise);
-);
